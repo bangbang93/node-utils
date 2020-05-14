72e188294f189fef4aa9650a46d68c2fb2a067bd
bangbang93 <bangbang93@163.com>
2020-05-14 18:20:23 +0800

    生成swagger

--- nestjs.ts
+++ nestjs.ts
@@ -1,3 +1,27 @@
-import {ApiOperation} from '@nestjs/swagger'
+import {NestFactory} from '@nestjs/core'
+import {ApiOperation, DocumentBuilder, SwaggerModule} from '@nestjs/swagger'
+import {writeFileSync} from "fs"
+import * as path from "path"
 
 export const ApiSummary = (summary: string) => ApiOperation({summary})
+
+interface IOptions {
+  title?: string
+  description?: string
+  version?: string
+}
+
+export async function generateSwagger(appModule, options?: IOptions) {
+  const app = await NestFactory.create(appModule);
+
+  const builder = new DocumentBuilder()
+    .setTitle(options?.title)
+    .setDescription(options?.description)
+    .setVersion(options?.version)
+    .build();
+  const document = SwaggerModule.createDocument(app, builder);
+  const outputPath = path.resolve(process.cwd(), 'swagger.json');
+  writeFileSync(outputPath, JSON.stringify(document), { encoding: 'utf8'});
+
+  await app.close();
+}
