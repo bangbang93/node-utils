5247e3f0346e21d6d48dfbb7a3efeafbc7846b06
bangbang93 <bangbang93@163.com>
2020-05-15 18:13:22 +0800

    add pagedto

--- nestjs.ts
+++ nestjs.ts
@@ -1,7 +1,8 @@
 import {NestFactory} from '@nestjs/core'
-import {ApiOperation, DocumentBuilder, SwaggerModule} from '@nestjs/swagger'
-import {writeFileSync} from "fs"
-import * as path from "path"
+import {ApiOperation, ApiPropertyOptional, DocumentBuilder, SwaggerModule} from '@nestjs/swagger'
+import {IsInt, IsOptional, Min} from 'class-validator'
+import {writeFileSync} from 'fs'
+import * as path from 'path'
 
 export const ApiSummary = (summary: string) => ApiOperation({summary})
 
@@ -24,4 +25,13 @@ export async function generateSwagger(appModule, options?: IOptions) {
   writeFileSync(outputPath, JSON.stringify(document), { encoding: 'utf8'});
 
   await app.close();
+}
+
+export class PagedDto {
+  @ApiPropertyOptional() @IsOptional() @IsInt() @Min(1) page: number = 1
+  @ApiPropertyOptional() @IsOptional() @IsInt() @Min(1) limit: number = 10
+
+  get skip() {
+    return (this.page - 1) * this.limit
+  }
 }
