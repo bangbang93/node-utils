8dbcaf585df5ea28c0ae5c3bbaf79935ef18698d
bangbang93 <bangbang93@163.com>
2020-05-25 10:15:42 +0800

    定义几个default

--- index.ts
+++ index.ts
@@ -3,3 +3,7 @@ import ms = require('ms')
 export function second(str: string): number {
   return ms(str) / 1000
 }
+
+export const DEFAULT_SKIP = 0
+export const DEFAULT_PAGE = 1
+export const DEFAULT_LIMIT = 10
