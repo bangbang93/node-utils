182e1ffb90f2bf3dc43771823ee1861b720cd606
bangbang93 <bangbang93@163.com>
2020-07-01 14:33:17 +0800

    给个between

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import ms = require('ms')
+import { max, min } from 'lodash'
 
 export function second(str: string): number {
   return ms(str) / 1000
@@ -7,3 +8,10 @@ export function second(str: string): number {
 export const DEFAULT_SKIP = 0
 export const DEFAULT_PAGE = 1
 export const DEFAULT_LIMIT = 10
+
+export function mongoBetween<T>(data: T[]): {$lte: T; $gte: T} {
+  return {
+    $lte: max(data),
+    $gte: min(data),
+  }
+}
