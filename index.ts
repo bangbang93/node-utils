4008cd62730b5061fe994f03b15b7dd8fbb201a4
bangbang93 <bangbang93@163.com>
2020-08-01 10:49:06 +0800

    优化withSession

--- index.ts
+++ index.ts
@@ -1,6 +1,4 @@
 import ms = require('ms')
-import { max, min } from 'lodash'
-import type { ClientSession } from 'mongoose'
 
 export function second(str: string): number {
   return ms(str) / 1000
@@ -9,26 +7,6 @@ export function second(str: string): number {
 export const DEFAULT_SKIP = 0
 export const DEFAULT_PAGE = 1
 export const DEFAULT_LIMIT = 10
-
-export function mongoBetween<T>(data: T[]): {$lte: T; $gte: T} {
-  return {
-    $lte: max(data),
-    $gte: min(data),
-  }
-}
-
-export async function withSession(
-  fn: (session: ClientSession) => Promise<void>,
-  startSession: (...args: unknown[]) => Promise<ClientSession>,
-  session?: ClientSession | null,
-  ): Promise<void> {
-  const useExists = !!session
-  if (useExists) {
-    return fn(session)
-  }
-  session = await startSession()
-  await session.withTransaction(fn)
-}
 
 export type Fn = (...args: unknown[]) => unknown
 export interface Constructor<T = unknown> {
