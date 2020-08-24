5c3dca058878519f2b1fb55acfae8a9d415286cd
bangbang93 <bangbang93@163.com>
2020-08-24 13:41:31 +0800

    还是给个函数

--- validator.ts
+++ validator.ts
@@ -1,4 +1,4 @@
 import {Transform} from 'class-transformer'
 import {toObjectId} from './mongodb'
 
-export const ToMongoId: PropertyDecorator = Transform(toObjectId)
+export const ToMongoId: () => PropertyDecorator = () => Transform(toObjectId)
