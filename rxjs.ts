c0717a34fb1c0ed1edc47ee664cb55a94bb39e19
bangbang93 <bangbang93@163.com>
2021-05-17 17:02:36 +0800

    add PromiseToObservable

--- rxjs.ts
+++ rxjs.ts
@@ -11,3 +11,7 @@ export function fromAsyncIter<T>(iter: AsyncIterable<T>): Observable<T> {
       .catch((err) => subscriber.error(err))
   })
 }
+
+export declare type PromiseToObservable<T> = {
+  [TKey in keyof T]: T[TKey] extends (...args: infer TArgs) => PromiseLike<infer TResult> ? (...args: TArgs) => Observable<TResult> : T[TKey];
+};
