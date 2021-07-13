0fcb42f4778f4d179c69b63ef88a1d7c631a0689
bangbang93 <bangbang93@163.com>
2021-07-13 14:34:23 +0800

    升级nest和rxjs

--- rxjs.ts
+++ rxjs.ts
@@ -1,15 +1,8 @@
-import {Observable} from 'rxjs'
+import {from, Observable, of} from 'rxjs'
 
+/** @deprecated just use from */
 export function fromAsyncIter<T>(iter: AsyncIterable<T>): Observable<T> {
-  return new Observable(subscriber => {
-    Promise.resolve().then(async () => {
-      for await (const data of iter) {
-        subscriber.next(data)
-      }
-      subscriber.complete()
-    })
-      .catch((err) => subscriber.error(err))
-  })
+  return from(iter)
 }
 
 export declare type PromiseToObservable<T> = {
