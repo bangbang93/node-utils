97017204107547c73abad861ffbc41cb11a74d5c
bangbang93 <bangbang93@163.com>
2022-01-14 10:22:21 +0800

    add ToBoolean decorator

--- validator.ts
+++ validator.ts
@@ -1,4 +1,7 @@
 import {Transform} from 'class-transformer'
+import {toBoolean} from './index'
 import {toObjectId} from './mongodb'
 
 export const ToMongoId: () => PropertyDecorator = () => Transform(({value}) => toObjectId(value))
+
+export const ToBoolean: () => PropertyDecorator = () => Transform(({value}) => toBoolean(value))
