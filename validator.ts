8027fe20f4761d77e600bcf983e9cfd04edc3ad4
bangbang93 <bangbang93@163.com>
2022-03-14 11:14:33 +0800

    给个trim

--- validator.ts
+++ validator.ts
@@ -1,7 +1,10 @@
 import {Transform} from 'class-transformer'
+import {trim} from 'lodash'
 import {toBoolean} from './index'
 import {toObjectId} from './mongodb'
 
 export const ToMongoId: () => PropertyDecorator = () => Transform(({value}) => toObjectId(value))
 
 export const ToBoolean: () => PropertyDecorator = () => Transform(({value}) => toBoolean(value))
+
+export const Trim: () => PropertyDecorator = () => Transform(({value}) => trim(value))
