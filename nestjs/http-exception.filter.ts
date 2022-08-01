33f1e7e8b1472a93eabe00059c3e015a32c346d6
bangbang93 <bangbang93@163.com>
2022-08-01 17:08:13 +0800

    优化异常处理

--- nestjs/http-exception.filter.ts
+++ nestjs/http-exception.filter.ts
@@ -1,10 +1,11 @@
-import {ArgumentsHost, Catch, ExceptionFilter, Inject, OnModuleInit} from '@nestjs/common'
+import {ArgumentsHost, Catch, ExceptionFilter, HttpException, Inject, OnModuleInit} from '@nestjs/common'
 import {ConfigService} from '@nestjs/config'
 import {stdSerializers} from 'bunyan'
 import {Request, Response} from 'express'
 import * as stringify from 'json-stringify-safe'
 import {omit, pick} from 'lodash'
 import {InjectLogger} from 'nestjs-bunyan'
+import {VError} from 'verror'
 import Logger = require('bunyan')
 
 @Catch()
@@ -60,20 +61,55 @@ export class HttpExceptionFilter implements ExceptionFilter, OnModuleInit {
     }
 
     if (res.headersSent) return
-    if (this.env?.toLowerCase() !== 'production') {
-      res.status(status)
-        .type('json')
-        .end(stringify({
+
+    let resp: string
+    if (err instanceof HttpException) {
+      const r = err.getResponse()
+      if (typeof r === 'string') {
+        resp = r
+      } else if (r instanceof Error) {
+        resp = this.getResponse(r)
+      } else {
+        resp = stringify({
+          ...pick(err, 'message', 'name', 'stack'),
+          ...r,
+        })
+      }
+    } else {
+      resp = this.getResponse(err)
+    }
+
+    res.status(status)
+      .type('json')
+      .end(resp)
+  }
+
+  private getResponse(err: Error): string {
+    if (err instanceof VError) {
+      const info = VError.info(err)
+      if (this.env?.toLowerCase() !== 'production') {
+        return stringify({
+          ...pick(err, 'message', 'name', 'stack'),
+          ...info,
+        })
+      } else {
+        return stringify({
+          ...pick(err, 'message', 'name'),
+          ...info,
+        })
+      }
+    } else {
+      if (this.env?.toLowerCase() !== 'production') {
+        return stringify({
           ...pick(err, 'message', 'name', 'stack'),
           ...err,
-        }))
-    } else {
-      res.status(status)
-        .type('json')
-        .end(stringify({
+        })
+      } else {
+        return stringify({
           ...pick(err, 'message', 'name'),
           ...omit(err, 'stack'),
-        }))
+        })
+      }
     }
   }
 }
