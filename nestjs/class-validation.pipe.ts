d264771d3530fcf6a01560bd3841038830a88558
bangbang93 <bangbang93@163.com>
2023-03-03 16:34:19 +0800

    feat: add class-validation.pipe.ts

---
+++ nestjs/class-validation.pipe.ts
@@ -0,0 +1,25 @@
+import {createError} from '@bangbang93/service-errors'
+import {Injectable, ValidationPipe as NestValidationPipe} from '@nestjs/common'
+import {ValidationPipeOptions} from '@nestjs/common/pipes/validation.pipe'
+import {ValidationError} from 'class-validator'
+
+/**
+ * 比起 NestJS 默认的 ValidationPipe，这个 ValidationPipe 会将错误信息转换为 ServiceErrors
+ */
+@Injectable()
+export class ClassValidationPipe extends NestValidationPipe {
+  constructor(options?: ValidationPipeOptions) {
+    super({
+      transform: true,
+      whitelist: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+      exceptionFactory: (errors: ValidationError[]) => {
+        const msgs = this.flattenValidationErrors(errors)
+        return createError.COMMON_MISSING_ARGUMENTS(msgs.join())
+      },
+      ...options,
+    })
+  }
+}
