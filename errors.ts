94e6256fd628555d45bf1c2b9978e1db6a0d6990
bangbang93 <bangbang93@163.com>
2024-05-29 13:59:08 +0800

    refactor: caughtError不应当给ServiceError

--- errors.ts
+++ errors.ts
@@ -1,4 +1,3 @@
-import {createError} from '@bangbang93/service-errors'
 import is from '@sindresorhus/is'
 import stringify from 'json-stringify-safe'
 import {isNil} from 'lodash'
@@ -10,8 +9,8 @@ export function caughtError(e: unknown): Error {
   }
   if (is.object(e)) {
     if ('message' in e && !isNil(e.message)) {
-      return createError.COMMON_UNKNOWN(e['message'] as string)
+      return new Error(e['message'] as string)
     }
   }
-  return createError.COMMON_UNKNOWN(stringify(e))
+  return new Error(stringify(e))
 }
