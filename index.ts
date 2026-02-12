8c9382e920bc3376280490dd116d8c03047533f0
bangbang93 <bangbang93@163.com>
2026-02-12 10:58:12 +0800

    fix: 更新toBoolean函数的类型定义以支持null和undefined，并优化代码格式

--- index.ts
+++ index.ts
@@ -4,7 +4,9 @@ import ms from 'ms'
 import {deprecate} from 'util'
 
 export function second(str: string): number {
-  return ~~(ms(str) / 1000)
+  return ~~(
+    ms(str) / 1000
+  )
 }
 
 export const DEFAULT_SKIP = 0
@@ -12,10 +14,13 @@ export const DEFAULT_PAGE = 1
 export const DEFAULT_LIMIT = 10
 
 export type Fn = (...args: unknown[]) => unknown
+
 export interface Constructor<T = unknown> {
   prototype: Prototype
+
   new(...args: unknown[]): T
 }
+
 export type Prototype = object
 
 export function Deprecated(message: string): MethodDecorator {
@@ -36,7 +41,7 @@ export interface Paged<T> {
 export type ValueOf<T> = T[keyof T]
 
 export function toBoolean(value: string | number | boolean): boolean
-export function toBoolean(value: null | undefined): null | undefined
+export function toBoolean(value: string | number | boolean | null | undefined): boolean | null | undefined
 export function toBoolean(value: string | number | boolean | null | undefined): boolean | null | undefined {
   if (is.nullOrUndefined(value)) return value
   return [true, 'true', '1', 'yes', 1].includes(value)
@@ -46,9 +51,11 @@ export function trimDeep<T extends object>(obj: T): T {
   return mapValues(obj, (v) => {
     if (is.string(v)) {
       return v.trim()
-    } if (is.plainObject(v)) {
+    }
+    if (is.plainObject(v)) {
       return trimDeep(v)
-    } return v
+    }
+    return v
   }) as T
 }
 
